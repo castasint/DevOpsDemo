a4a58d587733841f1d6c640af67ca24ced8c45e3
castasint <phanuma@deloitte.com>
2021-09-01 14:23:22 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevOpsDemo.Controllers
+    {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+        {
+        private IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+            {
+            this.configuration = configuration;
+            }
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IActionResult Get()
+            {
+        
+            var defaultLogLevel = this.configuration["Logging:LogLevel:Default"];
+            var environment = this.configuration["Environment"];
+            var firstName = this.configuration["Admin:FirstName"];
+            var lastName = this.configuration["Admin:LastName"];
+
+
+            return Content(
+                           $"Default Log Level: {defaultLogLevel} \n" +
+                           $"Environemt: {environment} \n" +
+                           $"First Name: {firstName} \n" +
+                            $"Last Name: {lastName} \n" );
+            }
+
+        }
+    }
